0f71dd147122fc3b099c610cb6369a461414907f
core-hacked <me@corehacked.cf>
2022-04-07 22:04:51 +0200

    Display the favicon correctly lmao :alien:
    change ``src="favicon"`` to ``href``

--- 403/index.php
+++ 403/index.php
@@ -49,7 +49,7 @@ else {
 <head>
 <link rel="stylesheet" href="../assets/main.css">
 <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-<link rel="icon" src="favicon.png">
+<link rel="icon" href="../favicon.png">
 <meta name="viewport" content="width=device-width, initial-scale=1" /> 
 <title>corehacked.cf</title>
 </head>
